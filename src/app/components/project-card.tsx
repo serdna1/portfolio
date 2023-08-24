da5325d0c589edb5afff0805a74a041a93535ab0
serdna1 <andrescaot@gmail.com>
2023-08-25 01:36:54 +0200

    Add responsiveness to the projects section

--- src/app/components/project-card.tsx
+++ src/app/components/project-card.tsx
@@ -3,18 +3,18 @@ import type { ProjectCardPropType } from "@/types"
 
 export const ProjectCard = ({screenshot, name, description, technologies, deployUrl, repoUrl}: ProjectCardPropType) => {
   return (
-    <article className='group relative hover:scale-105 hover:z-10'>
+    <article className='group lg:relative lg:hover:scale-105 lg:hover:z-10'>
       <img
         src={screenshot}
         alt="project screenshot"
-        className='aspect-square object-cover group-hover:grayscale'
+        className='aspect-square object-cover lg:group-hover:grayscale'
       />
-      <section className='bg-black/70 absolute top-0 w-full h-full hidden group-hover:flex flex-col justify-center text-center p-2 gap-4'>
+      <section className='lg:bg-black/70 lg:absolute lg:top-0 w-full lg:h-full lg:hidden lg:group-hover:flex flex flex-col justify-center text-center p-2 gap-4'>
         <div className='flex flex-col justify-center grow gap-4'>
-          <header className='text-white text-xl font-semibold'>
+          <header className='text-black lg:text-white text-xl font-semibold'>
             {name}
           </header>
-          <p className='text-white'>
+          <p className='text-black lg:text-white'>
             {description}
           </p>
           <div className='flex justify-center gap-2 text-3xl'>
--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -4,10 +4,10 @@ import { ProjectCard } from './project-card'
 
 export const Projects = () => {
   return (
-    <section className="flex flex-col gap-6 py-10 px-2 pt-10 bg-pink-200">
-      <h1 className="text-5xl font-bold text-center">Proyectos</h1>
+    <section className="flex flex-col items-center gap-6 py-10 px-2 pt-10 bg-pink-200">
+      <h1 className="text-5xl font-bold">Proyectos</h1>
       <p>filtros</p>
-      <section className='grid grid-cols-[repeat(auto-fill,minmax(320px,1fr))] gap-2'>
+      <section className='grid grid-cols-[repeat(auto-fill,minmax(320px,1fr))] gap-2 w-full'>
         {
           (projects as ProjectType[]).map((project) => (
             <ProjectCard 
