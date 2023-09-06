36bba19e13dbccec9e19f770c843609ec9f7f414
serdna1 <andrescaot@gmail.com>
2023-09-06 21:57:32 +0200

    Go back to have only the previous layout file but specify the metadata by hand in the returned html of the component to be able to deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,26 @@
-import type { Metadata } from 'next'
-import ClientLayout from './components/client-layout'
+"use client"
+
+import { ThemeProvider } from '@/context/theme'
+import { Navbar } from '@/app/components/navbar'
 
 import './globals.css'
 
-export const metadata: Metadata = {
-  title: 'leprechaunOTD',
-  description: 'Portfolio de leprechaunOTD',
-}
-
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <>
-      <ClientLayout>
-        {children}
-      </ClientLayout>
-    </>
+    <html 
+      lang="es"
+      className={`scroll-smooth ${(localStorage.getItem('darkMode') === null) ? window.matchMedia('(prefers-color-scheme: dark)').matches && 'dark' : (localStorage.getItem('darkMode') === 'true') && 'dark'}`}
+    >
+      <head>
+        <title>leprechaunOTD</title>
+        <meta name='description' content='Portfolio de leprechaunOTD' />
+      </head>
+      <body className='dark:text-white'>
+        <ThemeProvider>
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
   )
 }
